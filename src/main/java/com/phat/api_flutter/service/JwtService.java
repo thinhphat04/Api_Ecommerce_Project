0d4ed89f5129d3bb4f9c0cfad206e7f489d7f767
kaitokid1403 <dotridung94@gmail.com>
2024-08-11 23:52:38 +0700

    Fix error login

--- src/main/java/com/phat/api_flutter/service/JwtService.java
+++ src/main/java/com/phat/api_flutter/service/JwtService.java
@@ -24,11 +24,12 @@ public class JwtService {
 
     private String createToken(Map<String, Object> claims, String userName) {
         return Jwts.builder()
-                .setHeaderParam("typ", "JWT")
+
                 .setClaims(claims)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .setHeaderParam("typ", "JWT")
                 .compact();
     }
 
